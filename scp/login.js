3d2db59d802820c9d4219cf1570b43b48c048d98
Sebastian Sereno <46801087+Leialoha@users.noreply.github.com>
2022-05-23 23:04:03 -0700

    Update login.js

--- scp/login.js
+++ scp/login.js
@@ -1,37 +1,43 @@
-
-document.getElementById('login-2VoZ6Z2EJhtXUkzIW5HO').onclick = () => {
-    var USERNAME = document.getElementById('username-BP5CAEHcztbpS78TvhJ4').value;
-    var PASSWORD = document.getElementById('password-eaiWgIRzao1ilxm70eHt').value;
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        var data = JSON.parse(this.responseText);
-
-        document.getElementById("message-AlS18vmmXD4qT7t2yImG").className = "";
-        if (this.status != 200) {
-            document.getElementById("message-AlS18vmmXD4qT7t2yImG").classList.add('error-lK7SB5Vdb1rNC7f1psHc')
-            document.getElementById("message-AlS18vmmXD4qT7t2yImG").innerText = data.message;
-        }
-        
-        CheckLogin();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+router.get('/', async (req, res) => {
+    try {
+    if (req.query.username == undefined) {
+        console.log('No Username')
+        return res.status(400).json({"message": "Invalid username or password"});
     }
-    xhttp.open("GET", "/api/login?username=" + USERNAME + "&password=" + PASSWORD);
-    xhttp.send();
-};
-
-function CheckLogin() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        var data = JSON.parse(this.responseText);
-        if (this.status == 200) {
-            document.getElementById('login-MHLnNs8WDS9esw6TUErB').classList.add('hidden-2lt3nJhcHvEEjpz5aYNL');
-            document.getElementById('dash-5s11z7CXEp2dZFrpmLrm').classList.remove('hidden-2lt3nJhcHvEEjpz5aYNL');
-
-            LoadDashboard();
-        }
+    if (req.cookies.auth != undefined) {
+        var username = Buffer.from(req.cookies.auth, 'base64').toString('ascii').split('.').shift();
+        if (username == req.query.username.toUpperCase() && require.main.exports.CheckAuth(req.cookies.auth)) 
+            return res.status(200).json({"message": "Successfully logged in"});
     }
-    xhttp.open("GET", "/api/login/check");
-    xhttp.send();
-}
-
-CheckLogin();
+    var file = fs.readFileSync(await path.join(process.env.PWD, 'users.json'))
+    var data = JSON.parse(file);
+    var USER = data[req.query.username.toUpperCase()];
+    if (USER == undefined || !(USER.PASSWORD == undefined || USER.PASSWORD == '' || (req.query.password != undefined && bcrypt.compareSync(req.query.password, USER.PASSWORD)))) {
+        console.log('Invalid username or password')
+        return res.status(400).json({"message": "Invalid username or password"});
+    }
+    console.log('Logged in')
+    res.status(200).cookie('auth', require.main.exports.GenerateAuth(req.query.username.toUpperCase(), req.query.password)).json({"message": "Successfully logged in"})
+    } catch (error) {
+	console.log(error)
+        return res.status(500).json({"message": error.message});
+    }
+});
+router.get('/check', (req, res) => {
+    if (req.cookies.auth != undefined && require.main.exports.CheckAuth(req.cookies.auth))
+        return res.status(200).json({"message": "Logged In"});
+    
+    res.status(400).json({"message": "Not Logged In"});
+});
+router.get('/generate', async (req, res) => {
+    if (req.query.password == undefined)
+        return res.status(400).json({"message": "Please provide a password"});
+    res.status(200).json({"message": bcrypt.hashSync(req.query.password, saltRounds)})
+});
+module.exports = router;
